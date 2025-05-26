62f5435d479ff0734af4974e8e2f43b12efdac8c
shvgpt-070208 <shvgpt070208@gla.ac.in>
2025-05-26 13:20:59 +0530

    intor commit

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='w-screen fixed bg-transparent backdrop-blur-xs p-4 flex justify-between'>
-        <div className=' z-20'>Clothify</div>
+    <div className='w-screen fixed bg-transparent backdrop-blur-[1.4px] p-4 flex justify-between'>
+        <div className=' z-20 text-[max(1.8vw,20px)] font-bold text-white font-stretch-50% font-serif uppercase'>Clothify</div>
         <div className='flex'>
           </div>   
     </div>
