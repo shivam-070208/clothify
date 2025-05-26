5e70068ebb197f4b944e09460a37148a773d50e1
shvgpt-070208 <shvgpt070208@gla.ac.in>
2025-05-26 11:02:56 +0530

    first commit

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='w-screen flex justify-between'>
-        <div className='text-xl'>Clothify</div>
+        <div className='texter'>Clothify</div>
       
     </div>
   )
