fb9ab9edc019930a97e99255ae9f14261fb63800
shvgpt-070208 <shvgpt070208@gla.ac.in>
2025-05-27 13:08:29 +0530

    heroic commit

--- src/component/Introhomepage.jsx
+++ src/component/Introhomepage.jsx
@@ -4,7 +4,7 @@ const Introhomepage = () => {
   return (
     <div className='w-full max-[680px]:columns-1 columns-2 bg-blue-600 rounded-t-xl'>
       <div className=' w-full min-h-[60vh] pt-10  flex flex-col pl-9 pr-0  rounded-l-xl '>
-        <h1 className='font-extrabold   text-[max(4vw,34px)] font-serif text-white leading-[max(3.3vw,16px)]'>Bold Your <br /> Style Everyday</h1>
+        <h1 className='font-extrabold   text-[max(4vw,34px)] font-serif text-white leading-[max(3.3vw,28px)]'>Bold Your <br /> Style Everyday</h1>
         <p className=' text-[max(1.2vw,14px)] mt-7 text-zinc-300  font-bold font-sans  min-[420px]:w-[50%] w-[90%] leading-[max(1vw,14px)]'>Shop Our latest collection for everyday wear and standout looks,tailred to fit your lifestyles. </p>
       </div>
       <div className=' w-full rounded-r-xl min-h-[60vh] grid place-items-center'>Column 2</div>
