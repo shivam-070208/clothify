a96ac93e6184015160b9e5b60bb2f1370feefad0
shvgpt-070208 <shvgpt070208@gla.ac.in>
2025-05-29 08:24:28 +0530

    final commit

--- src/app/showcase/Utils.js
+++ src/app/showcase/Utils.js
@@ -2,43 +2,43 @@ export const SellData=[
     {
         Path:"/Models/japanese_clothes.glb",
         img:"/images/i0.jpg",
-        Tittle:"Jackobelly",
-        Content:"A well handmade japanesse Belly clothes designed by the OkaloMunasa.",
+        Tittle:"JackoWoman",
+        Content:"A well handmade japanesse Woman clothes designed by the OkaloMunasa.",
         price:"29000"
     },
       {
         Path:"/Models/jackets.glb",
         img:"/images/i1.jpg",
         Tittle:"Tankuasha",
-        Content:"A well handmade japanesse Belly clothes designed by the OkaloMunasa.",
+        Content:"A well handmade japanesse Woman clothes designed by the OkaloMunasa.",
         price:"22000"
     },
     {
         Path:"/Models/tshapedClothes.glb",
         img:"/images/i3.jpg",
         Tittle:"Tuashir",
-        Content:"A well handmade japanesse Belly clothes designed by the OkaloMunasa.",
+        Content:"A well handmade japanesse Woman clothes designed by the OkaloMunasa.",
         price:"22000"
     },
       {
         Path:"/Models/japanese_clothes.glb",
         img:"/images/i0.jpg",
-        Tittle:"Jackobelly",
-        Content:"A well handmade japanesse Belly clothes designed by the OkaloMunasa.",
+        Tittle:"JackoWoman",
+        Content:"A well handmade japanesse Woman clothes designed by the OkaloMunasa.",
         price:"29000"
     },
       {
         Path:"/Models/jackets.glb",
         img:"/images/i1.jpg",
         Tittle:"Tankuasha",
-        Content:"A well handmade japanesse Belly clothes designed by the OkaloMunasa.",
+        Content:"A well handmade japanesse Woman clothes designed by the OkaloMunasa.",
         price:"22000"
     },
     {
         Path:"/Models/tshapedClothes.glb",
         img:"/images/i3.jpg",
         Tittle:"Tuashir",
-        Content:"A well handmade japanesse Belly clothes designed by the OkaloMunasa.",
+        Content:"A well handmade japanesse Woman clothes designed by the OkaloMunasa.",
         price:"22000"
     }
 ]
